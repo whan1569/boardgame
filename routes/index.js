57dd5a647c924eaba200ae12bed9282dc6e7d2b3
whan1569 <whan4404@naver.com>
2021-01-26 04:12:07 +0900

    대화신청 추가
    대화신청, 대화목록 index에서 조회가능

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 var express = require('express');
+const dbcon = require("../config/db_con");
 var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  a="hello";
+router.get('/',async function(req, res, next) {
   if(req.session.user)
   {
-    a="hello  " + req.session.user.user_id;
+    var user_id = req.session.user.user_id;
+    var result = await dbcon("SELECT user_1,user_2  FROM talk WHERE user_1 = ? OR user_2 = ?",[user_id,user_id]);
+    
   }
   else
     res.redirect("/users/login");
-  res.render('index', { title:  a});
+  res.render('index', { user_id : user_id , result : result});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -50,6 +50,28 @@ router.get('/room_list', async function(req, res, next) {
   res.render('room_list', { title: 'room_list', room : room});
 });
 
+router.get('/room_list/talk', async function(req, res, next) {
+  user_1 = req.session.user.user_id;
+  user_2 = req.query.user_id;
+  console.log(user_1 + "\n" +user_2);
+  if(user_1 == user_2)
+  {
+    res.redirect("/users/room_list");
+  }
+  else
+  {
+    if(user_1 > user_2)
+    {
+      var imsi;
+      imsi = user_1;
+      user_1 = user_2;
+      user_2 = imsi;
+    }
+    await dbcon("INSERT INTO boardgame.talk(`user_1`, `user_2`) VALUES (?, ?);",[user_1,user_2]);
+    res.redirect("/");
+  }
+});
+
 router.get('/room_insert', function(req, res, next) {
   res.render('room_insert', { title: 'room_insert' });
 });
